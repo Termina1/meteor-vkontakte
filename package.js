8991315fcfd2b05c6675767954d5ccb9bb381a50
Termina1 <terminal2010@gmail.com>
2014-02-23 22:16:17 +0400

    fix

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: "Meteor wrapper for node-vkontakte"
 });
-Npm.depends({feedparser: "~0.3.0"});
+Npm.depends({vkontakte: "~0.3.0"});
 Package.on_use(function(api){
     api.add_files('meteor-vkontakte.js', 'server');
     if(api.export)
