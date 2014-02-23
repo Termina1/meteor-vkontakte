2b5f3461bddbf64bc174c6294e9cfc9e53a9c4bb
Termina1 <terminal2010@gmail.com>
2014-02-23 22:00:53 +0400

    initial commit

---
+++ package.js
@@ -0,0 +1,9 @@
+Package.describe({
+    summary: "Meteor wrapper for node-vkontakte"
+});
+Npm.depends({feedparser: "~0.3.0"});
+Package.on_use(function(api){
+    api.add_files('meteor-vkontakte.js', 'server');
+    if(api.export)
+        api.export('Vkontakte');
+});
